88dbf9f20ecc457ba5659cb50dfd9cb5d9ea5a1f
liav <liav916@gmail.com>
2024-01-29 15:17:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/*
+import Tests.ArticlePrint;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(ArticlePrint.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed successfully.");
+        } else {
+            System.out.println("Some tests failed.");
+        }
+    }
+}
+*/
+
+
+
+
+
